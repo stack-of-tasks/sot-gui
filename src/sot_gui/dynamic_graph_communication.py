75d826eff3f7fe93f8d34ca94a17bf90773d6240
Justine Fricou <justine.fricou@gmail.com>
2022-06-07 13:52:13 +0200

    Removing option to run code locally

--- src/sot_gui/dynamic_graph_communication.py
+++ src/sot_gui/dynamic_graph_communication.py
@@ -6,42 +6,13 @@ class DynamicGraphCommunication():
     """ This class allows to communicate with a SoT dynamic graph, which can
         be local or on a remote server.
     """
-    _is_local = False
-
-
-    @staticmethod
-    def dg_is_local() -> None:
-      DynamicGraphCommunication._is_local = True
-
 
     def __init__(self):
         self._client = SOTClient()
-        self._local_vars  = {}
-        self._global_vars = {}
         self._import_dynamic_graph()
 
 
     def run(self, code: str, ret_value: bool = True) -> Any:
-        """ Runs code, either locally or on the remote server. """
-        # TODO: should the code be encoded?
-        if DynamicGraphCommunication._is_local:
-          return self.run_local_code(code, ret_value)
-        else:
-          return self.run_remote_code(code, ret_value)
-
-
-    def run_local_code(self, code: str, ret_value: bool) -> Any:
-        """ Runs code locally, and returns a value if needed. """
-        local_vars = None
-        global_vars = self._global_vars
-        if ret_value:
-          exec(f"_returnValue = {code}", global_vars, local_vars)
-          return eval("_returnValue", global_vars, local_vars)
-        else:
-          exec(code, global_vars, local_vars)
-
-
-    def run_remote_code(self, code: str, ret_value: bool) -> Any:
         """ Runs code on the remote server, and returns a value if needed. """
         try:
             # TODO: reconnect if the server was restarted
@@ -61,7 +32,7 @@ class DynamicGraphCommunication():
 
         except: # TODO: try to reconnect?
             ...
-
+        
 
     def _import_dynamic_graph(self) -> None:
         """ Imports the dynamic graph. """
